943bb01b398fa26fc51f70c7ba547f4a60e9bf39
Swathi <swathisak16@gmail.com>
2023-07-09 08:32:36 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProfileComponent } from './page/profile/profile.component';
+import { ProductsComponent } from './page/products/products.component';
+import { ShoppingCartComponent } from './page/shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './page/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
